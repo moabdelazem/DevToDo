641d30dfd95f9b35327a36ea85ff759c8016d875
Mohamed Abdelazem <mabdelazemahmed@gmail.com>
2024-11-26 17:22:57 +0200

    edit the database config

--- app/db/database.py
+++ app/db/database.py
@@ -1,8 +1,11 @@
 from motor.motor_asyncio import AsyncIOMotorClient
+from decouple import config
 
 # MongoDB connection string
-MONGO_DETAILS = "mongodb://root:example@localhost:27017"
-DATABASE_NAME = "devtodo"
+MONGO_DETAILS = config(
+    "MONGO_DETAILS", default="mongodb://root:example@localhost:27017"
+)
+DATABASE_NAME = config("DATABASE_NAME", default="devtodo")
 
 # Instantiate a client
 client = AsyncIOMotorClient(MONGO_DETAILS)
